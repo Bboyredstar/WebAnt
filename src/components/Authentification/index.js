e9514b19c1c8243740865c359b8c9181e93c1bdf
Timofey Leonov <mbdoy213@gmail.com>
2019-12-18 23:04:15 +0300

    18/12/19 получен access & refresh_token, добоавлена тестовая проверка ввода имени и пароля

--- src/components/Authentification/index.js
+++ src/components/Authentification/index.js
@@ -1,80 +1,83 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import './style.css'
 import Preloader from '../Images/Preloader'
 
-const Authentification = ()=> {
+const SignIn = ()=> {
 
     const [userName,setUserName] = useState('')
     const [userPasswd,setUserPasswd] = useState('')
-    const [authError,setAuthError] = useState(true)
+    const [isUserInputDataWrong,setInputError] = useState('')
+    const [errorTokenDescription,setDescription] = useState('')
+    const [isAuthErrored,setAuthError] = useState(true)
     const [accessToken,setAccessToken] = useState('')
-    const [refresh_token,setrefreshToken] = useState('')
+    const [refreshToken,setrefreshToken] = useState('')
     const [clientId,setClientId] = useState('')
     const [clientSecret,setClientSecret] = useState('')
-    const [isLoading,setLoading] = useState(true)
+    const [isLoading,setLoading] = useState(false)
+    
     useEffect(()=>{
-        getClientId()
+      getClientId() 
     },[])
 
-    
-    
     const getClientId = async () => {
-      try{
-        const response= await Axios.post('api/clients',{
-         "name": "string",
-         "allowedGrantTypes": [
-         "password", "refresh_token"
-         ]
-     }) 
+      try {
+        const response = await Axios.post('api/clients',{
+          "name": "string",
+          "allowedGrantTypes": [
+          "password", "refresh_token"]
+        }) 
         setClientId(response.data.id + '_' + response.data.randomId)
         setClientSecret(response.data.secret)
-        setLoading(false)
-        }
-      catch(e){
-        console.log(e)
-        }
-    }
-      
-      const gettingToken = async () =>{
-        try{
-          const data = await Axios({
-                url: '/oauth/v2/token',
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept': 'application/json',
-                },
-                data: {
-                  grant_type:'password',
-                  username: 'testiruy',
-                  password: 'password',
-                  client_id: clientId,
-                  client_secret: clientSecret,
-                },
-                maxContentLength: 2000,
-            }
-          )
-        }
-        //http://gallery.dev.webant.ru/oauth/v2/token?grant_type=password&client_id=840_54akov57sps0s0k0ogcockwskkw0o84ck0o08sw00gwcwco808&client_secret=2nb49hgzvoow40okkso0cc80osso00sc48swcgs88swwgkgccc&username=testiruy&password=password
-      catch(err){
-        console.log(err)
+      }
+      catch(error){
+        console.log(error.response.data.error_description)
       }
     }
-
-
-
-
- return(
+      const gettingToken = async () =>{
+        try{
+          const data = await Axios.get('oauth/v2/token',{
+            params:{
+              'grant_type':'password',
+              'client_id':clientId,
+              'client_secret':clientSecret,
+              'username':userName,
+              'password':userPasswd
+            }
+          })
+          setrefreshToken(data.refresh_token)
+          setAccessToken(data.access_token)
+          
+        }
+        catch(error) {
+            setDescription(error.response.data.error_description);
+          }
+    }
+     const validation = (data)=>{
+        const regex = /[A-Za-z0-9]{6,}/
+        return !regex.test(data)
+          ? "The input data must contain at least six letters or numbers."
+          : "";
+      }
+      const dataValidation = (value)=>{
+        const error = validation(value)
+        setInputError(error)
+        if(!error){
+          setAuthError(false)
+        }       
+      }
+    
+ 
+return(
     <div className='authform'>
-     {isLoading ? <Preloader/>: <form>
-          <input type='text' className='authform__input' maxLength = '20' pattern='[A-Za-z]' placeholder='Username' />
-          <input type='password' className='authform__input' minLength='6' maxLength='12' placeholder='Password' onChange={(e)=>console.log(e.target.value)}/>
-          {authError && <button type='button' className = 'authform_submit' onClick = {gettingToken}>Log In</button>}
-      </form>}
-    </div>
- )
- }
+      <form>
+          {isUserInputDataWrong && <div>{isUserInputDataWrong}</div>}
+          <input type='text' className='authform__input' maxLength = '20' pattern='[A-Za-z]' placeholder='Username' onBlur={(e)=>dataValidation(e.target.value)}/>
+          <input type='password' className='authform__input' minLength='6' maxLength='12' placeholder='Password' onBlur={(e)=>dataValidation(e.target.value)}/>
+          {!isAuthErrored && <button type='button' className = 'authform_submit' onClick = {gettingToken}>Log In</button>}
+      </form>
+    </div>)
+  
+}
 
-
-export default Authentification
+  export default SignIn
