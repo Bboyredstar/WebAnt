f93c3b3c7600ccb15fa146aeff45c5208ce35e2e
Timofey Leonov <mbdoy213@gmail.com>
2019-12-19 20:33:34 +0300

    19/12/19 Сделана авторизация

--- src/components/Authentification/index.js
+++ src/components/Authentification/index.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import './style.css'
-import Preloader from '../Images/Preloader'
-
 const SignIn = ()=> {
 
     const [userName,setUserName] = useState('')
     const [userPasswd,setUserPasswd] = useState('')
-    const [isUserInputDataWrong,setInputError] = useState('')
     const [errorTokenDescription,setDescription] = useState('')
-    const [isAuthErrored,setAuthError] = useState(true)
     const [accessToken,setAccessToken] = useState('')
     const [refreshToken,setrefreshToken] = useState('')
     const [clientId,setClientId] = useState('')
     const [clientSecret,setClientSecret] = useState('')
-    const [isLoading,setLoading] = useState(false)
     
     useEffect(()=>{
       getClientId() 
@@ -31,7 +26,7 @@ const SignIn = ()=> {
         setClientSecret(response.data.secret)
       }
       catch(error){
-        console.log(error.response.data.error_description)
+        setDescription('Sorry some problem with server, try again later!')
       }
     }
       const gettingToken = async () =>{
@@ -45,39 +40,30 @@ const SignIn = ()=> {
               'password':userPasswd
             }
           })
-          setrefreshToken(data.refresh_token)
-          setAccessToken(data.access_token)
-          
+          settingTokens(data.data.access_token,data.data.refresh_token)
+          setDescription('')
         }
         catch(error) {
-            setDescription(error.response.data.error_description);
+            setDescription(error.response.data.error_description)
           }
     }
-     const validation = (data)=>{
-        const regex = /[A-Za-z0-9]{6,}/
-        return !regex.test(data)
-          ? "The input data must contain at least six letters or numbers."
-          : "";
+    const settingTokens = (accessToken,refreshToken) =>{
+      if ((accessToken != undefined) && (refreshToken != undefined)){
+        localStorage.setItem('accessToken',accessToken)
+        localStorage.setItem('refreshToken',refreshToken)
       }
-      const dataValidation = (value)=>{
-        const error = validation(value)
-        setInputError(error)
-        if(!error){
-          setAuthError(false)
-        }       
-      }
+    }
     
  
 return(
     <div className='authform'>
       <form>
-          {isUserInputDataWrong && <div>{isUserInputDataWrong}</div>}
-          <input type='text' className='authform__input' maxLength = '20' pattern='[A-Za-z]' placeholder='Username' onBlur={(e)=>dataValidation(e.target.value)}/>
-          <input type='password' className='authform__input' minLength='6' maxLength='12' placeholder='Password' onBlur={(e)=>dataValidation(e.target.value)}/>
-          {!isAuthErrored && <button type='button' className = 'authform_submit' onClick = {gettingToken}>Log In</button>}
+          <input type='text' className='authform__input' maxLength = '20' placeholder='Username' onChange={(e)=>setUserName(e.target.value)}/>
+          <input type='password' className='authform__input' minLength='6' maxLength='12' placeholder='Password' onChange={(e)=>setUserPasswd(e.target.value)}/>
+          {errorTokenDescription && <div className='auth__error'>{errorTokenDescription}</div>}
+          <button type='button' className = 'authform_submit' onClick = {gettingToken}>Log In</button>
       </form>
     </div>)
-  
-}
+} 
 
   export default SignIn
